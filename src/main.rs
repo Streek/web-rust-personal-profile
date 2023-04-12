e4892923ddc0eef11e41235f95e792824372819b
Keith Connolly <streek@mesaro.com>
2023-04-12 08:39:38 -0700

    update

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,32 @@ extern crate dotenv;
 use dotenv::dotenv;
 use rocket::fs::FileServer;
 use rocket_dyn_templates::Template;
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
+struct App {
+    name: String,
+    description: String,
+    image: String,
+    link: String,
+}
+
+fn create_app_list() -> Vec<App> {
+    vec![
+        App {
+            name: "App 1".to_string(),
+            description: "A description of App 1".to_string(),
+            image: "app1.png".to_string(),
+            link: "https://example.com/app1".to_string(),
+        },
+        App {
+            name: "App 1".to_string(),
+            description: "A description of App 1".to_string(),
+            image: "app1.png".to_string(),
+            link: "https://example.com/app1".to_string(),
+        },
+    ]
+}
 
 #[get("/")]
 fn index() -> Template {
@@ -14,11 +40,18 @@ fn index() -> Template {
     Template::render("index", &context)
 }
 
+#[get("/apps")]
+fn applications() -> Template {
+    let context: Vec<App> = create_app_list();
+    Template::render("apps", &context)
+}
+
 #[launch]
 fn rocket() -> _ {
     dotenv().ok();
     rocket::build()
         .mount("/", routes![index])
+        .mount("/apps", routes![applications])
         .mount("/images", FileServer::from("./images"))
         .attach(Template::fairing())
 }
